8815caed5e140f4cd9b03c00c85dbea0b1ec99d2
Pablo Reiser <prkubli@gmail.com>
2022-05-04 19:38:44 +0800

    Initial Commit

---
+++ LearningGH2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGH2
+//
+//  Created by Pablo Reiser on 04.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGH2/LearningGH2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGH2App.swift
+//  LearningGH2
+//
+//  Created by Pablo Reiser on 04.05.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
